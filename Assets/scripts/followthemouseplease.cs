696f515868b93163671129482546739b621691bb
Maya Wolfe <miniwolfe2@gamil.com>
2025-01-09 15:10:26 -0500

    circle following mouse position code

---
+++ Assets/scripts/followthemouseplease.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followthemouseplease : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousPos;
+    }
+}

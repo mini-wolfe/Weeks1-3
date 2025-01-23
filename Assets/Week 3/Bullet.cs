21ff9454bc0f5924ada110f3ca58a46600efa3b4
Maya Wolfe <miniwolfe2@gmail.com>
2025-01-23 13:23:12 -0500

    public

--- Assets/Week 3/Bullet.cs
+++ Assets/Week 3/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 5;
-    bool hasBeenFired = false;
+     public bool hasBeenFired = false;
     void Update()
     {
         PointAtMouse();

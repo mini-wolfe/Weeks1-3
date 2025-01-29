eebd04e51ea3f24d82a70426f5c4868335e464eb
MINIS-PC\miniw <miniwolfe2@gmail.com>
2025-01-29 15:41:19 -0500

    creating heart beat code, making it reversaable, so that the heart doesnt hit minimum size instead always stays visible.

--- Assets/assigment one/heartbeat.cs
+++ Assets/assigment one/heartbeat.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class heartbeat : MonoBehaviour
 {
+   [Range(0,1)]
+   public float t;
+   public float reverse;
+
     public bool checkmouseclick;
+    public AnimationCurve pulse;
 
     // Start is called before the first frame update
     void Start()
     {
         checkmouseclick = false; 
+        t = 0.1f;
+        reverse = 0.3f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(t>=1 || t <= 0){
+        reverse *= -1;
+        }
        if(Input.GetMouseButtonDown(0))
        {
            checkmouseclick = true; 
           
        }
       if(checkmouseclick == true){
-           Debug.Log("Hi");
+
+      t += reverse * Time.deltaTime; 
+
+      transform.localScale = Vector2.one * pulse.Evaluate(t);
       }
         
     }
